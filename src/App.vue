e391d1321b50167b3840c3106e1f56feaad7a6a3
Quentin <quentin.senelas@gmail.com>
2020-10-28 10:57:36 +0100

    Stable build
    Ajout des différentes pages avec router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<nav>
+			<router-link to="/">
+				<div class="navigation__logo">
+					Twotter
+				</div>
+			</router-link>
+			<div class="navigation__user">
+				{{ state.user.username }}
+			</div>
+		</nav>
+		<router-view />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	setup() {
+		const state = reactive({
+			user: {
+				username: '_QuentinSenelas',
+			},
+		});
+
+		return {
+			state,
+		};
+	},
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	min-height: 100vh;
+	background-color: #f3f5fa;
+
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 10px 5%;
+		background-color: #42b983;
+		color: white;
+
+		.navigation__logo {
+			font-weight: bold;
+			font-size: 24px;
+		}
+
+		.navigation__user {
+			font-weight: bold;
+		}
+	}
 }
 </style>
